3d5298cf454fdfd68928cbb2596e0db6239ab887
sunpingji <pingji.spj@gmail.com>
2017-01-10 15:24:42 +0800

    multidex

--- app/src/main/java/pjsun/ticket/MainApplication.java
+++ app/src/main/java/pjsun/ticket/MainApplication.java
@@ -1,6 +1,7 @@
 package pjsun.ticket;
 
 import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.facebook.stetho.Stetho;
 
@@ -10,7 +11,7 @@ import org.litepal.LitePal;
  * Created by sunpi on 2016/12/22.
  */
 
-public class MainApplication extends Application {
+public class MainApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
